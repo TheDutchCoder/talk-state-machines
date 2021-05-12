dfcc2b29a9e9bfbcf489ee2a8f6c71f09870f000
Reinier Kaper <reinier@canvaspop.com>
2021-05-12 19:51:35 -0400

    feat: :sparkles: add starter component template

---
+++ src/components/Users.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Users</h2>
+  <ul>
+    <li>User 1</li>
+    <li>User 2</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+
+  setup () {
+    return {}
+  },
+}
+</script>
